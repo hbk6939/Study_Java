15d74fe7965353ca6987499ee85d3747ab5cde22
황병국 <hbk6939@gmail.com>
2023-01-06 12:56:23 +0000

    study java

--- src/h_Inheritance/f_polymorphism.java
+++ src/h_Inheritance/f_polymorphism.java
@@ -1,5 +1,31 @@
-// 다형성(polymorphism)이란?
-// 다형성(polymorphism)이란 하나의 객체가 여러 가지 타입을 가질 수 있는 것을 의미합니다.
-// 자바에서는 이러한 다형성을 부모 클래스 타입의 참조 변수로 자식 클래스 타입의 인스턴스를 참조할 수 있도록 하여 구현하고 있습니다.
-// 다형성은 상속, 추상화와 더불어 객체 지향 프로그래밍을 구성하는 중요한 특징 중 하나입니다.
+// 참조 변수의 타입 변환
+// 자바에서는 참조 변수도 다음과 같은 조건에 따라 타입 변환을 할 수 있습니다.
 
+// 1. 서로 상속 관계에 있는 클래스 사이에만 타입 변환을 할 수 있습니다.
+// 2. 자식 클래스 타입에서 부모 클래스 타입으로의 타입 변환은 생략할 수 있습니다.
+// 3. 하지만 부모 클래스 타입에서 자식 클래스 타입으로의 타입 변환은 반드시 명시해야 합니다.
+
+// 참조 변수의 타입 변환도 기본 타입의 타입 변환과 마찬가지로 타입 캐스트 연산자(())를 사용합니다.
+
+
+class Parent { }
+class Child extends Parent { }
+class Brother extends Parent { }
+
+
+public class f_polymorphism{
+    public static void main(String[] args) {
+        Parent pa01 = null;
+        Child ch = new Child();
+        Parent pa02 = new Parent();
+        Brother br = null;
+
+        pa01 = ch;          // pa01 = (Parent)ch; 와 같으며, 타입 변환을 생략할 수 있음.    
+        try {
+            br = (Brother)pa02; // 타입 변환을 생략할 수 없음.
+            // br = (Brother)ch;   // 직접적인 상속 관계가 아니므로, 오류 발생.
+        } catch (Exception e) {
+            System.out.println("오류 : " + e);
+        }
+    }
+}
---
+++ src/h_Inheritance/g_polymorphism2.java
@@ -0,0 +1,18 @@
+class Parent { }
+class Child extends Parent { }
+class Brother extends Parent { }
+
+public class g_polymorphism2 {
+	public static void main(String[] args) {
+		Parent p = new Parent();
+		System.out.println(p instanceof Object);	// true
+		System.out.println(p instanceof Parent);	// true
+		System.out.println(p instanceof Child);		// false
+		System.out.println();
+		
+		Parent c = new Child();
+		System.out.println(c instanceof Object);	// true
+		System.out.println(c instanceof Parent);	// true
+		System.out.println(c instanceof Child);		// true
+	}
+}
